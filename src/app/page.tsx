509f0fd8c494c01f82cf0d41ab8f6879f7ee45c7
nahyun park <pnh13584@gmail.com>
2025-03-21 21:25:37 +0900

    💄 style: 메인 페이지 레이아웃 css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
 export default function Home() {
-  return <div>메인 페이지</div>;
+  return (
+    <section className="p-5">
+      <article>
+        <p className="text-2xl font-bold">인기 순위</p>
+        <div className="flex flex-row w-full h-1/4 rounded-xl p-4 justify-between gap-4">
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+        </div>
+      </article>
+      <article>
+        <p className="text-2xl font-bold">카테고리 1</p>
+        <div className="flex flex-row w-full h-1/4 rounded-xl p-4 justify-between gap-4">
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+        </div>
+      </article>
+      <article>
+        <p className="text-2xl font-bold">카테고리 2</p>
+        <div className="flex flex-row w-full h-1/4 rounded-xl p-4 justify-between gap-4">
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+        </div>
+      </article>
+    </section>
+  );
 }
