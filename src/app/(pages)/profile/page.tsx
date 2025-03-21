262df41314377efbf1a627f7375027609dd67279
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-22 06:11:49 +0900

    🎉 add : 앨범 리스트 컴포넌트화 & 폴더 생성

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,112 +1,61 @@
-import Image from "next/image";
+// import Image from "next/image";
 import React from "react";
 import { AiOutlineEllipsis } from "react-icons/ai";
-import cat from "../../../public/cat.jpeg";
-import dog from "../../../public/dog.jpeg";
-import parrow from "../../../public/parrow.jpeg";
+import AlbumList from "../../../components/AlbumList";
+import type { album } from "../../../types/album";
+
+export const catData: album[] = [
+  {
+    title: "cat",
+    artist: "Dog",
+    image: "/../../public/cat.jpeg",
+  },
+  {
+    title: "dog",
+    artist: "Cat",
+    image: "/../../public/dog.jpeg",
+  },
+  {
+    title: "bird",
+    artist: "Parrow",
+    image: "/../../public/cat.jpeg",
+  },
+  {
+    title: "cat",
+    artist: "Dog",
+    image: "/../../public/cat.jpeg",
+  },
+  {
+    title: "dog",
+    artist: "Cat",
+    image: "/../../public/cat.jpeg",
+  },
+  {
+    title: "cat",
+    artist: "Dog",
+    image: "/../../public/cat.jpeg",
+  },
+  {
+    title: "dog",
+    artist: "Cat",
+    image: "/../../public/cat.jpeg",
+  },
+];
 
 function ProfilePage() {
-  const catData = [
-    {
-      title: "cat",
-      artist: "Dog",
-      image: cat,
-    },
-    {
-      title: "dog",
-      artist: "Cat",
-      image: dog,
-    },
-    {
-      title: "bird",
-      artist: "Parrow",
-      image: parrow,
-    },
-    {
-      title: "cat",
-      artist: "Dog",
-      image: cat,
-    },
-    {
-      title: "dog",
-      artist: "Cat",
-      image: dog,
-    },
-    {
-      title: "cat",
-      artist: "Dog",
-      image: cat,
-    },
-    {
-      title: "dog",
-      artist: "Cat",
-      image: dog,
-    },
-  ];
-
   return (
     <main className=" bg-zinc-950 rounded-2xl m-6 min-h-screen pb-10">
       <section className="w-full h-[200px] bg-gradient-to-b from-zinc-600 to-zinc-800 rounded-t-2xl flex flex-row items-center mb-8">
         <div className="w-[150px] h-[150px] bg-zinc-800 drop-shadow-md rounded-full ml-10"></div>
         <div className="text-white ml-10">
-          <p className="text-sm">프로필</p>
+          <p className="text-[14px]">프로필</p>
           <h1 className="text-7xl font-black mt-3">sm</h1>
-          <p className="text-sm font-normal mt-2">좋아요한 앨범 수 개</p>
+          <p className="text-[14px] font-normal mt-2">좋아요한 앨범 수 개</p>
         </div>
       </section>
-      <AiOutlineEllipsis className="text-zinc-400 text-4xl ml-10" />
-      <section className="ml-10">
-        <div className="my-16">
-          <h2 className="text-white text-[26px] font-bold mb-5">
-            좋아요한 앨범
-          </h2>
-          <div className="flex flex-row gap-10 overflow-x-scroll flex-nowrap">
-            {catData.map(data => {
-              return (
-                <div key={data.title} className="text-white">
-                  <div className="w-[180px] h-[180px] overflow-hidden">
-                    <Image
-                      src={data.image}
-                      alt={"ImageAlt"}
-                      width={400}
-                      height={400}
-                      className="w-full h-full"
-                    />
-                  </div>
-                  <h3 className="text-xl font-bold">{data.title}</h3>
-                  <p className="text-sm font-thin">{data.artist}</p>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </section>
-      <section className="ml-10">
-        <div className="mt-20">
-          <h2 className="text-white text-[26px] font-bold mb-5">
-            좋아요한 앨범
-          </h2>
-          <div className="flex flex-row gap-10 overflow-x-scroll flex-nowrap">
-            {catData.map(data => {
-              return (
-                <div key={data.title} className="text-white">
-                  <div className="w-[180px] h-[180px] overflow-hidden">
-                    <Image
-                      src={data.image}
-                      alt={"ImageAlt"}
-                      width={400}
-                      height={400}
-                      className="w-full h-full"
-                    />
-                  </div>
-                  <h3 className="text-xl font-bold">{data.title}</h3>
-                  <p className="text-sm font-thin">{data.artist}</p>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </section>
+      <AiOutlineEllipsis className="text-zinc-400 text-4xl ml-10" />{" "}
+      <AlbumList albumdata={catData} />
+      <AlbumList albumdata={catData} />
     </main>
   );
 }
