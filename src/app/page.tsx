4ca7d609cfbbc3b7b9b1442c5d402e09d5688180
nahyun park <pnh13584@gmail.com>
2025-03-21 22:18:19 +0900

    💄 style: 메인 페이지 css 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    <section className="p-5">
+    <section className="p-5 flex flex-col gap-4">
       <article>
         <p className="text-2xl font-bold">인기 순위</p>
         <div className="flex flex-row w-full h-1/4 rounded-xl p-4 justify-between gap-4">
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
           <div className="w-40 h-40 rounded-md bg-white"></div>
           <div className="w-40 h-40 rounded-md bg-white"></div>
           <div className="w-40 h-40 rounded-md bg-white"></div>
@@ -17,11 +19,15 @@ export default function Home() {
           <div className="w-40 h-40 rounded-md bg-white"></div>
           <div className="w-40 h-40 rounded-md bg-white"></div>
           <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
         </div>
       </article>
       <article>
         <p className="text-2xl font-bold">카테고리 2</p>
         <div className="flex flex-row w-full h-1/4 rounded-xl p-4 justify-between gap-4">
+          <div className="w-40 h-40 rounded-md bg-white"></div>
+          <div className="w-40 h-40 rounded-md bg-white"></div>
           <div className="w-40 h-40 rounded-md bg-white"></div>
           <div className="w-40 h-40 rounded-md bg-white"></div>
           <div className="w-40 h-40 rounded-md bg-white"></div>
