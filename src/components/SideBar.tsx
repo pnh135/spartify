921e13e9998cc3c02356dba656934cbf0b4c8c1b
nahyun park <pnh13584@gmail.com>
2025-03-21 22:09:20 +0900

    🩹 fix: 주석 수정

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -21,6 +21,7 @@ function SideBar() {
             <p className="text-bg-primary text-sm">Playlist - 120songs</p>
           </div>
         </div>
+        {/* 구현하지 않기로 한 좋아요 목록 이외 플레이리스트지만 틀만 잡아둠  */}
         <div className="flex w-full bg-slate-200 p-2">
           <div className="bg-gray-500 w-1/4 h-10">
             <p className="text-xs p-2">image</p>
