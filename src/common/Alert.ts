9239fc830470991e5f85ad275004a7a90c568568
nahyun park <pnh13584@gmail.com>
2025-03-22 17:49:28 +0900

    🎉 add: Alert 파일 생성

---
+++ src/common/Alert.ts
@@ -0,0 +1,48 @@
+import Swal from "sweetalert2";
+
+export const AlertSuccess = (title = "Success", text = "") => {
+  Swal.fire({
+    icon: "success",
+    title,
+    text,
+    customClass: {
+      popup: "swal-custom",
+      icon: "swal-icon",
+    },
+  });
+};
+
+export const AlertError = (title = "Error", text = "") => {
+  Swal.fire({
+    icon: "error",
+    title,
+    text,
+  });
+};
+
+export const AlertInfo = (title = "Info", text = "") => {
+  Swal.fire({
+    icon: "info",
+    title,
+    text,
+  });
+};
+
+export const AlertConfirm = Swal.fire({
+  title: "정말 삭제하시겠습니까?",
+  text: "삭제한 후에는 복구가 불가능합니다",
+  icon: "warning",
+
+  showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+  confirmButtonColor: "#d33", //빨간색
+  cancelButtonColor: "#3085d6", //파란색
+  confirmButtonText: "삭제",
+  cancelButtonText: "취소",
+  reverseButtons: true, // 버튼 순서 거꾸로
+}).then(result => {
+  if (result.isConfirmed) {
+    Swal.fire("삭제가 완료되었습니다.", "", "success");
+  } else if (result.isDenied) {
+    Swal.fire("삭제를 취소하셨습니다.", "", "info");
+  }
+});
