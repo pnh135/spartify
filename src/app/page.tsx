e8728ea10dc53bcf6da1c1ace06b181c4fb8f58f
nahyun park <pnh13584@gmail.com>
2025-03-22 17:54:00 +0900

    🩹 fix: 변수이름 오류 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
 import AlbumList from "@/components/AlbumList";
 import { catData } from "./(pages)/profile/page";
-import { album } from "@/types/album";
 
 export default function Home() {
-  const albumData: album[] = catData;
   return (
     <section className="p-5 flex flex-col gap-4">
       <article>
-        <AlbumList albumdata={albumData} />
+        <AlbumList albumData={catData} albumListName={"좋아요한 곡"} />
       </article>
       <article>
         <p className="text-2xl font-bold">카테고리 1</p>
         {/* 추후 삭제될 태그 */}
-        <AlbumList albumdata={albumData} />
+        <AlbumList albumData={catData} albumListName={"카테고리1"} />
       </article>
       <article>
         <p className="text-2xl font-bold">카테고리 2</p>
         {/* 추후 삭제될 태그 */}
-        <AlbumList albumdata={albumData} />
+        <AlbumList albumData={catData} albumListName={"카테고리2"} />
       </article>
     </section>
   );
