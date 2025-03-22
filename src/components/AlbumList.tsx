4316b1439a5061f8fbaa21a1d4b83c3468b4ba90
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-22 15:10:46 +0900

    ✨ update  : 페이지 제목을 파라미터로 받아올 수 있게 수정

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -1,32 +1,39 @@
 import Image from "next/image";
 // import { catData } from "@/app/(pages)/profile/page";
-import type { album } from "@/types/album";
+import type { album } from "../types/album";
+import Link from "next/link";
 
 interface AlbumListProps {
   albumdata: album[];
+  albumListName: string;
 }
 
-export default function AlbumList({ albumdata }: AlbumListProps) {
+export default function AlbumList({
+  albumdata,
+  albumListName,
+}: AlbumListProps) {
   return (
     <section className="ml-8">
       <div className="my-16">
-        <h2 className="text-white text-2xl font-bold mb-5">좋아요한 앨범</h2>
+        <h2 className="text-white text-2xl font-bold mb-5">{albumListName}</h2>
         <div className="flex flex-row gap-8 overflow-x-scroll flex-nowrap">
-          {albumdata.map(data => {
+          {albumdata.map(album => {
             return (
-              <div key={data.title} className="text-white">
-                <div className="w-[180px] h-[180px] overflow-hidden mb-2">
-                  <Image
-                    src={data.image}
-                    alt={"ImageAlt"}
-                    width={400}
-                    height={400}
-                    className="w-full h-full bg-gray-500"
-                  />
-                </div>
-                <h3 className="text-md">{data.title}</h3>
-                <p className="text-sm">{data.artist}</p>
-              </div>
+              <Link key={album.title} href={`/detail/${album.title}`}>
+                <article className="text-white">
+                  <figure className="w-[180px] h-[180px] overflow-hidden mb-2">
+                    <Image
+                      src={album.image}
+                      alt={"ImageAlt"}
+                      width={400}
+                      height={400}
+                      className="w-full h-full bg-gray-500"
+                    />
+                  </figure>
+                  <h3 className="text-md">{album.title}</h3>
+                  <p className="text-sm">{album.artist}</p>
+                </article>
+              </Link>
             );
           })}
         </div>
