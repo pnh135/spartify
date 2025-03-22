5bfeba0b604a45bb3840906065287b030b342047
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-22 16:05:47 +0900

    merge dev into branch profile

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function SideBar() {
   return (
     // 모든 내용은 로그인 전제
-    <section className="m-2 mt-4 p-4 bg-container-bg-primary border border-transparent rounded-2xl h-full">
+    <section className="m-2 mt-4 p-4 bg-gunmetal border border-transparent rounded-2xl h-full">
       <div className="flex flex-row justify-between">
         <p className="text-2xl">My Playlists</p>
         {/* 버튼의 경우 추후 삭제될 가능성 존재 */}
@@ -17,8 +17,8 @@ function SideBar() {
             <p className="text-xs p-2">image</p>
           </div>
           <div>
-            <p className="text-bg-primary font-semibold">Liked Songs</p>
-            <p className="text-bg-primary text-sm">Playlist - 120songs</p>
+            <p className="text-charcoal font-semibold">Liked Songs</p>
+            <p className="text-charcoal text-sm">Playlist - 120songs</p>
           </div>
         </div>
         {/* 구현하지 않기로 한 좋아요 목록 이외 플레이리스트지만 틀만 잡아둠  */}
@@ -27,8 +27,8 @@ function SideBar() {
             <p className="text-xs p-2">image</p>
           </div>
           <div>
-            <p className="text-bg-primary font-semibold">설레는 노래</p>
-            <p className="text-bg-primary text-sm">Playlist - 30songs</p>
+            <p className="text-charcoal font-semibold">설레는 노래</p>
+            <p className="text-charcoal text-sm">Playlist - 30songs</p>
           </div>
         </div>
       </article>
