6d8efd40ee04cb5fe2367c833bc26c076f57f838
bluepome98 <wpdn1998@gmail.com>
2025-03-23 13:32:44 +0900

    🎉 add 공통적으로 보여줄 스포티파이 API 키 설정 시작

--- src/app/api/spotify/album/[id]/route.ts
+++ src/app/api/spotify/album/[id]/route.ts
@@ -1,53 +1,21 @@
-// /app/api/spotify/album/[id]/route.ts
-import { NextRequest, NextResponse } from "next/server";
-import { SpotifyAlbum } from "@/types/album";
+{
+  /*Get New Releases API 레퍼런스를 통해 데이터보이기
+  메인페이지에서 20개의 앨범 데이터 보이기
+  */
+}
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } },
-) {
-  try {
-    // Step 1. access token 가져오기
-    const tokenRes = await fetch("http://localhost:3000/api/spotify/token");
-    const tokenData = await tokenRes.json();
-    const accessToken = tokenData.access_token;
+const clientId = process.env.SPOTIFY_CLIENT_ID!;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET!;
+const basicAuth = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
 
-    // Step 2. Spotify 앨범 데이터 요청 (미국 지역 기준)
-    const albumRes = await fetch(
-      `https://api.spotify.com/v1/albums/${params.id}?market=US`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      },
-    );
+const PUBLIC_TOKEN_URL='https://accounts.spotify.com/api/token'
 
-    const albumRaw = await albumRes.json();
-
-    // Step 3. 필요한 데이터만 추출해서 파싱
-    const parsedAlbum: SpotifyAlbum = {
-      name: albumRaw.name,
-      images: albumRaw.images,
-      artists: albumRaw.artists.map((artist: any) => ({
-        name: artist.name,
-        id: artist.id,
-      })),
-      tracks: {
-        items: albumRaw.tracks.items.map((track: any) => ({
-          name: track.name,
-          id: track.id,
-          duration_ms: track.duration_ms,
-          preview_url: track.preview_url,
-        })),
-      },
-    };
-
-    return NextResponse.json(parsedAlbum);
-  } catch (error) {
-    console.error("[Spotify Album API Error]", error);
-    return NextResponse.json(
-      { error: "Failed to fetch album" },
-      { status: 500 },
-    );
-  }
+export async function getPublicAccessToken(){
+  const res=await fetch(PUBLIC_TOKEN_URL,{
+    method:"POST",
+    headers:{
+      Authorization:`Basic ${basicAuth}`,
+      "Content-Type":'applicatiobn'
+    }
+  })
 }
--- src/app/api/spotify/token/route.ts
+++ src/app/api/spotify/token/route.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from "next/server";
 
+const APP_TOKEN_URL = "https://accounts.spotify.com/api/token";
+
+
 export async function GET() {
   const basicAuth = Buffer.from(
     `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`,
   ).toString("base64");
 
-  const res = await fetch("https://accounts.spotify.com/api/token", {
+  const res = await fetch(APP_TOKEN_URL, {
     method: "POST",
     headers: {
       Authorization: `Basic ${basicAuth}`,
