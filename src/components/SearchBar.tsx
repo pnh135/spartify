1efd60e0c660b786a65ed1e5738f173946f92e6d
nahyun park <pnh13584@gmail.com>
2025-03-26 02:40:50 +0900

    🧪 test: 수정 로직 테스트

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,53 +1,60 @@
 "use client";
 
-// components/SearchBar.tsx
+import { getPublicAccessToken } from "@/app/api/spotify/route";
 import { SpotifySearch } from "@/types/album";
 // import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const SearchBar = () => {
   const [searchData, setSearchData] = useState("");
   const [results, setResults] = useState<SpotifySearch[]>([]);
   const [loading, setLoading] = useState(false);
 
-  const getSearch = async e => {
-    e.preventDefault();
-    if (!searchData) return;
-
-    setLoading(true);
-
-    try {
-      const res = await fetch(
-        `https://api.spotify.com/v1/search?q=${searchData}&type=album`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: "Bearer " + `${token}`,
-          },
+  const getResults = async () => {
+    const accessToken = await getPublicAccessToken();
+    const res = await fetch(
+      `https://api.spotify.com/v1/search?q=${searchData}&type=album&limit=10&offset=0`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + `${accessToken}`,
         },
-      );
-      const data = await res.json();
-      setResults(data);
-      console.log(data);
-    } catch (error) {
-      console.error("Failed to fetch search results", error);
+      },
+    );
+    if (!res.ok) {
+      throw new Error("불러오기 오류");
     }
 
-    setLoading(false);
+    const data: SpotifySearch[] = await res.json();
+    setResults(data);
+  };
+
+  useEffect(() => {
+    if (!searchData) {
+      return;
+    }
+    getResults();
+  }, []);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "0") {
+      return;
+    }
+    setSearchData(e.target.value);
   };
 
   return (
     <div>
-      <form onSubmit={e => getSearch(e)}>
+      <form onSubmit={getResults}>
         <input
           type="text"
           value={searchData}
-          onChange={e => setSearchData(e.target.value)}
+          onChange={e => handleInputChange(e)}
           placeholder="What do you want to play?"
           className="bg-gunmetal border border-transparent rounded-full h-10 px-10 focus:ring-2 focus:ring-inset focus:ring-gray-600 focus:outline-none hidden md:block"
         />
         <button type="submit" disabled={loading}>
-          {loading ? "Searching..." : "Search"}
+          {loading ? "검색중..." : "검색"}
         </button>
       </form>
 
