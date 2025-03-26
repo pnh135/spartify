0ee2a65bb1a6838e0c2566792125dbac4e62eb72
answq <wnffldksla@gmail.com>
2025-03-26 14:51:07 +0900

    ⚒fix: 앨범 상세페이지 필요없는 콘솔로그 삭제

--- src/app/(pages)/detail/[id]/page.tsx
+++ src/app/(pages)/detail/[id]/page.tsx
@@ -7,7 +7,7 @@ import LikeBtn from "@/components/LikeBtn";
 
 async function AlbumDetailPage({ params }: { params: { id: string } }) {
   const album = await getAlbum(params.id);
-  console.log(album);
+  //console.log(album);
   return (
     <main className=" bg-zinc-950 rounded-2xl m-4 sm:m-6 min-h-screen pb-10">
       <section className=" flex flex-col  from-zinc-600 to-zinc-800 rounded-t-2xl  items-center mb-8 mx-4 sm:mx-8 ">
