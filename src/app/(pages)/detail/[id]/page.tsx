f960c82daf47a5dda530d3973879808a5bde2e03
bluepome98 <wpdn1998@gmail.com>
2025-03-26 10:35:16 +0900

    🩹 fix UI 수정

--- src/app/(pages)/detail/[id]/page.tsx
+++ src/app/(pages)/detail/[id]/page.tsx
@@ -9,20 +9,20 @@ async function AlbumDetailPage({ params }: { params: { id: string } }) {
   const album = await getAlbum(params.id);
   console.log(album.tracks);
   return (
-    <main className=" bg-zinc-950 rounded-2xl m-4 sm:m-6 min-h-screen pb-10">
-      <section className=" flex flex-col  from-zinc-600 to-zinc-800 rounded-t-2xl  items-center mb-8 mx-4 sm:mx-8 ">
+    <main className=" bg-zinc-950 rounded-2xl m-4 sm:m-6 min-h-screen pb-4">
+      <section className=" flex flex-col  from-zinc-600 to-zinc-800 rounded-t-2xl  items-center mb-4 mx-4 sm:mx-8 ">
         <Image
           priority
-          quality={10}
+          quality={100}
           src={album.images?.[0]?.url}
           alt={album.name}
-          width={350}
-          height={350}
-          className=" bg-white rounded-sm  p-2 m-8"
+          width={360}
+          height={360}
+          className=" bg-white rounded-sm  p-2 m-4"
         />
         <div className="flex flex-col justify-between text-white w-full p-4 ">
-          <h1 className="text-2xl font-black mt-3">{album.name}</h1>
-          <p className="opacity-40 pt-2">{album.artists[0].name}</p>
+          <h1 className="text-4xl font-black m-2">{album.name}</h1>
+          <p className="opacity-40 m-2">{album.artists[0].name}</p>
           <LikeBtn albumId={params.id} />
         </div>
       </section>
