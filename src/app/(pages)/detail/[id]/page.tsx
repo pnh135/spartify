fbf960803bcb64126382a94da532b75530752c3b
answq <wnffldksla@gmail.com>
2025-03-26 23:10:42 +0900

    🎉add: 댓글창 컴포넌트로 생성, comments 테이블 생성 후 타입 재설정, 댓글창, 좋아요버튼 ui 임시 수정

--- src/app/(pages)/detail/[id]/page.tsx
+++ src/app/(pages)/detail/[id]/page.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Image from "next/image";
 import { getAlbum } from "@/app/api/spotify/route";
-// import { SpotifyTrack } from "@/types/track";
 import TrackList from "@/components/TrackList";
 import LikeBtn from "@/components/LikeBtn";
+import CommentList from "@/components/CommentList";
 
 async function AlbumDetailPage({ params }: { params: { id: string } }) {
   const album = await getAlbum(params.id);
-  console.log(album.tracks);
   return (
     <main className=" bg-zinc-950 rounded-2xl m-4 sm:m-6 min-h-screen pb-4">
       <section className=" flex flex-col  from-zinc-600 to-zinc-800 rounded-t-2xl  items-center mb-4 mx-4 sm:mx-8 ">
@@ -18,18 +17,20 @@ async function AlbumDetailPage({ params }: { params: { id: string } }) {
           alt={album.name}
           width={360}
           height={360}
-          className=" bg-white rounded-sm  p-2 m-4"
+          className="bg-white rounded-sm p-2 m-4"
         />
-        <div className="flex flex-col justify-between text-white w-full p-4 ">
+        <div className="flex flex-col justify-between text-white w-full p-4">
           <h1 className="text-4xl font-black m-2">{album.name}</h1>
           <p className="opacity-40 m-2">{album.artists[0].name}</p>
           <LikeBtn albumId={params.id} />
         </div>
       </section>
-
-      <div className="p-4">
+      <section className="p-4">
         <TrackList tracks={album.tracks.items} />
-      </div>
+      </section>
+      <section className="p-4">
+        <CommentList albumId={params.id} />
+      </section>
     </main>
   );
 }
