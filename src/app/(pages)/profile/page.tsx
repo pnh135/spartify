8959a7ad77489ffbb2a1aa999d3090d067c6a5a1
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-27 01:51:51 +0900

    ✨ update  : 수정된 유저정보 바로 반영되도록 기능 구현

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -11,6 +11,7 @@ import UserSettingModal from "@/components/UserSettingModal";
 import type { SpotifyAlbum } from "@/types/album";
 import { getUserInfo } from "@/utils/getUserData";
 import { handleUpdateUserData } from "@/utils/updateUserData";
+import Image from "next/image";
 
 //useEffect 최대한 없애고 tanstackQuery하기!!! 무조건!!!!!!!!!!!!!!무조건!!!!!!!!!!!!!
 export interface userType {
@@ -24,7 +25,7 @@ export interface userType {
 function ProfilePage() {
   const [optionToggle, setOptionToggle] = useState<boolean>(false);
   const [newRelease, setNewRelease] = useState<SpotifyAlbum[]>([]);
-  const [userName, setUserName] = useState<string>("");
+  const [userData, setUserData] = useState<string>("");
   const [profileSettingModal, setProfileSettingModal] =
     useState<boolean>(false);
 
@@ -41,11 +42,12 @@ function ProfilePage() {
   useEffect(() => {
     const fetchUser = async () => {
       const userInfo = await getUserInfo();
-      console.log("this this the userInfoooooooo", userInfo.name);
-      setUserName(userInfo.name);
+      console.log("this is userInfo======> ", userInfo);
+      console.log("thsi is urser_img===>", userData.profile_img);
+      setUserData(userInfo);
     };
     fetchUser();
-  }, [userName]);
+  }, [userData]);
 
   // 유저 상세 설정 버튼 토글 함수 (점 세개))
   const handleOptionToggle = () => {
@@ -63,7 +65,17 @@ function ProfilePage() {
     <main className="bg-zinc-950 rounded-2xl m-6 min-h-screen pb-10">
       <section className="w-full h-[250px] bg-gradient-to-b from-zinc-600 to-zinc-800 rounded-t-2xl flex flex-row items-center mb-8">
         <figure className="relative group w-[200px] h-[200px] flex justify-center items-center text-7xl text-zinc-500 bg-zinc-800 shadow-zinc-900 shadow-lg rounded-full ml-10">
-          <HiMiniUser className="opacity-100 group-hover:opacity-0" />
+          {/* <HiMiniUser className="opacity-100 group-hover:opacity-0" /> */}
+          {userData.profile_img === "" ? (
+            <HiMiniUser className="absolute opacity-100 group-hover:opacity-0" />
+          ) : // <Image
+          //   src={userData.profile_img}
+          //   alt={""}
+          //   width={300}
+          //   height={300}
+          //   className="w-[200px] h-[200px] rounded-full text-sm group-hover:opacity-20"
+          // />
+          null}
           <button
             type="button"
             onClick={handleProfileSetting}
@@ -76,7 +88,7 @@ function ProfilePage() {
         </figure>
         <section className="text-white ml-10">
           <p className="text-[14px]">프로필</p>
-          <h1 className="text-7xl h-20 font-black mt-3">{userName}</h1>
+          <h1 className="text-7xl h-20 font-black mt-3">{userData.name}</h1>
           <p className="text-[14px] font-normal mt-3">좋아요한 앨범 수 개</p>
         </section>
       </section>
